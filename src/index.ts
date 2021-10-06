7fe08100d96349265f8956cf2b5afe121fdd9fa8
Edvin Eriksson <edvinerikson@gmail.com>
2021-10-06 19:22:25 +0200

    remove old code

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export type Reference<T> = {
-  isLoaded: () => boolean;
-  load: () => Promise<T>;
-  get: () => T | null;
-}
-
-export function makeResource<T>(loader: () => Promise<T>): Reference<T> {
-  throw new Error("Expected this to be compiled");
-}
